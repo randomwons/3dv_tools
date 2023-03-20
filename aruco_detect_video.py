360ce70f3f0d24987768e3cc13325a8ac3c6c261
jinwon <cjinone2@gmail.com>
2023-03-20 17:22:49 +0900

    Add aruco_detect_video.py

---
+++ aruco_detect_video.py
@@ -0,0 +1,16 @@
+import cv2
+
+dictionary = cv2.aruco.getPredefineDictionary(cv2.aruco.DICT_6X6_250)
+
+video = cv2.VideoCapture(0)
+
+while True:
+    ret, image = video.read()
+    imagecopy = image.copy()
+    corners, ids, _ = cv2.aruco.detectMarkers(image, dictionary)
+    if ids is not None:
+        cv2.aruco.drawDetectedMarkers(imagecopy, corners, ids)
+    cv2.imshow("image", imagecopy)
+    if cv2.waitKey(1) == ord('q'):
+        break
+cv2.destroyAllWindows()
